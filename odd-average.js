dd647fdddb9eb518557273b436a2189fb1aa77cc
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-04 11:54:50 +0600

    odd average and event average function write done

--- odd-average.js
+++ odd-average.js
@@ -5,16 +5,37 @@
 
 function oddAverage(numbers){
     // console.log(numbers);
+    const oddList = [];
     let sum = 0;
     for(const number of numbers){
         // console.log(number);
         if(number % 2 !== 0){
+            oddList.push(number);
             sum += number;
         }
     }
     // console.log(oddList);
-    return sum;
+    let average = sum / oddList.length;
+    return average;
 }
 
-const digits = [35, 64, 73, 85, 95, 34, 64, 898, 4546];
+
+function evenAverage(numbers){
+    // console.log(numbers);
+    const evenList = [];
+    let sum = 0;
+    for(const number of numbers){
+        // console.log(number);
+        if(number % 2 === 0){
+            evenList.push(number);
+            sum += number;
+        }
+    }
+    // console.log(oddList);
+    let average = sum / evenList.length;
+    return average;
+}
+
+const digits = [35, 64, 73, 85, 95, 34, 64, 898, 4546, 245];
 console.log(oddAverage(digits));
+console.log(evenAverage(digits));
