fd7bc9e97a190e079be04461a343ef5ef6774e1b
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-04 14:14:28 +0600

    math operation

---
+++ math.js
@@ -0,0 +1,21 @@
+
+// const numbers = [1, 5, 61, 87, 7, 5, 81, 61];
+const min = Math.min(1, 5, 61, 87, 7, 5, 81, 61);
+// console.log(min);
+const max = Math.max(1, 5, 61, 87, 7, 5, 81, 61);
+// console.log(max);
+
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(5-10));
+// console.log(Math.round(2.25));
+// console.log(Math.round(2.85));
+// console.log(Math.floor(2.95));
+// console.log(Math.floor(2.9999));
+// console.log(Math.ceil(2.95));
+// console.log(Math.ceil(2.9999));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
