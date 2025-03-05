0d65b4f19817cdd6c9e0dd35f262ad83e058b82b
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-05 11:17:25 +0600

    date set

---
+++ date.js
@@ -0,0 +1,11 @@
+const today = new Date();
+const date = new Date('2062-10-09');
+// console.log(date.getMonth());
+// console.log(date.getDay());
+
+const specificDate = new Date(2091, 0, 26);
+specificDate.setMonth(10);
+// console.log(specificDate.toLocaleDateString('en-GB'));
+
+
+// unix epoc
